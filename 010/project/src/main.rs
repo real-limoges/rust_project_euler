05e0d717ed0112b6d9150eea76e51546f6d3a76a
Real Limoges <b.real.limoges@gmail.com>
2017-10-01 14:21:38 -0700

    Add structure for generating primes from 003

---
+++ 010/project/src/main.rs
@@ -0,0 +1,41 @@
+// This is lifting from some of my earlier code (003)
+fn is_prime(num: u64) -> bool {
+  for i in 2..(num / 2 + 1) {
+    if num % i == 0 {
+      return false;
+    }
+  }
+  return true;
+}
+
+// Struct to hold the current Prime
+struct PrimeNum {
+  curr: u64,
+}
+
+// Implements the Iterator for Prime numbers
+impl Iterator for PrimeNum {
+  type Item = u64;
+  fn next(&mut self) -> Option<u64> {
+    let mut new_next = self.curr + 1;
+    while !is_prime(new_next){
+      new_next += 1;
+    }
+    
+    self.curr = new_next;
+    
+    Some(self.curr);
+  }
+}
+
+// Create fuction to serve the primes over. initializes to 1
+fn primes() -> PrimeNum {
+  PrimeNum { curr: 1 }
+}
+
+
+fn main() {
+  println!("Hello World!");
+
+
+}
