f6bcd4db651378d98f6221482fe1a9306be6d5af
Real Limoges <b.real.limoges@gmail.com>
2017-10-06 06:07:13 -0700

    update using fixed size array manipulation rather than prime generator

--- 010/project/src/main.rs
+++ 010/project/src/main.rs
@@ -1,45 +1,30 @@
-// This is lifting from some of my earlier code (003)
-fn is_prime(num: u64) -> bool {
-  for i in 2..(num / 2 + 1) {
-    if num % i == 0 {
-      return false;
-    }
-  }
-  return true;
-}
-
-// Struct to hold the current Prime
-struct PrimeNum {
-  curr: u64,
-}
-
-// Implements the Iterator for Prime numbers
-impl Iterator for PrimeNum {
-  type Item = u64;
-  fn next(&mut self) -> Option<u64> {
-    let mut new_next = self.curr + 1;
-    while !is_prime(new_next){
-      new_next += 1;
-    }
-    
-    self.curr = new_next;
-    
-    Some(self.curr)
-  }
-}
-
-// Create fuction to serve the primes over. initializes to 1
-fn primes() -> PrimeNum {
-  PrimeNum { curr: 2 }
-}
-
-
 fn main() {
-  let mut summer: u64 = 0;
-  let mut counter: u64 = 0; 
-  for i in primes() {
-    if i < 2000000 { summer += i; counter += 1; }
-    else { break; } 
+  
+  let mut summer = 0;
+  const SIZE: usize = 2000000;
+
+  // fixed array of size SIZE initialized to true
+  let mut slots: [bool; SIZE] = [true; SIZE];
+ 
+  // initially mark 0 and 1 as not prime 
+  slots[0] = false;
+  slots[1] = false;
+
+  // for each number, 
+  for jump in 2..(SIZE/2) {
+    // Current starting position. Mark off every multiple as false
+    let mut pos = jump;
+    while pos < (SIZE - jump) {
+      pos += jump;
+      slots[pos] = false;
+    }
   }
-  println!("Sum is {} with {} primes", summer, counter)
+
+  for (index, prime) in slots.into_iter().enumerate(){
+    if *prime {
+      summer += index;
+    }
+  }
+
+  println!("The Sum is {:?}", summer);
 }
